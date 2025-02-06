4881a8e9c268bf0f15560101959d45000440b477
zrwinte <zrwinte@ilstu.edu>
2025-02-06 16:28:50 -0600

    Update Reciever.ino

--- Reciever.ino
+++ Reciever.ino
@@ -1,5 +1,5 @@
 #include <SoftwareSerial.h>
-#include <LiquidCrystal.h> // Include the LCD library
+#include <LiquidCrystal.h>
 #include "rgb_lcd.h"
 #include <Wire.h>
 
@@ -9,17 +9,17 @@ SoftwareSerial mySerial(6,7); // RX, TX
 
 
 void setup() {
-Serial.begin(9600); // Initialize hardware serial connection
-mySerial.begin(9600); // Initialize SoftwareSerial connection
-lcd.begin(16, 2); // Initialize LCD (assuming 16x2 display)
+Serial.begin(9600); 
+mySerial.begin(9600);
+lcd.begin(16, 2); 
 lcd.print("Waiting...");
 }
 
 void loop() {
-if (mySerial.available() > 0) { // If information is ready to be read...
+if (mySerial.available() > 0) { 
 String receivedMessage = mySerial.readString();
 lcd.clear();
-lcd.print(receivedMessage); // Display on LCD
-mySerial.println(receivedMessage); // Print to Serial Monitor for debugging
+lcd.print(receivedMessage); 
+mySerial.println(receivedMessage); 
 }
 }
